1132bda7e5af29a3ed2fbfcc289da9fe6f286e8e
bonnie blue <bluebonnie8@gmail.com>
2025-02-04 14:36:23 +0700

    Reapply "Views Deleted."
    This reverts commit aee5f8d47b0d5265303b2c8597967cb4b20b8062.

---
+++ TestTask1/Models/DTO/DTONewOrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTask1.Models.DTO
+{
+    public class DTONewOrderItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
---
+++ TestTask1/Models/Provider.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask1.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
